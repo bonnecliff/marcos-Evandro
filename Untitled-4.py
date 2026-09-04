92ac860c8ac8113381f067958fbe9bf2671e20af
unknown <aluno@IBC-LAB01-23.CETAM.LOCAL>
2026-09-04 19:26:09 -0400

    envio de arquivos

---
+++ Untitled-4.py
@@ -0,0 +1,16 @@
+# exemplo de conversão texto -> float
+numero1 = input( "Digite o primeiro numero: ")
+print(f"o numero digitado foi:{numero1}")
+
+numero2 = float (numero1)
+print(f"o numero convertido para float é:{numero2}")
+
+#exemplo de if , if , elif, else
+idade = 14
+if idade>= 18:
+    print("voto obrigatorio!")
+elif idade >= 16:
+    print("voto facultativo!")
+else:
+    print("não vota")
+    
---
+++ idade_atual =50.py
@@ -0,0 +1,8 @@
+idade_atual =50
+idade_futura = idade_atual + 5
+
+print ("idade atual:", idade_atual)
+print ("daqui a 5 anos terá:",idade_futura, "anos")
+
+
+
