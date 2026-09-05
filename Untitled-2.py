f610d044b1df5e493517cbb41cc44f9a90212d33
unknown <aluno@IBC-LAB01-23.CETAM.LOCAL>
2026-09-04 20:22:45 -0400

    envio de arquivo

---
+++ Untitled-2.py
@@ -0,0 +1,9 @@
+numero1 =input("Digite o primeiro numero:")
+print (f"o numéro digitado foi:{numero1}")
+
+numero2 = float(numero1)
+print(f"o numéro convertido para float é:{numero2}")
+
+#exemplo de impressao de float com zeros
+valor =1.200
+print (f"o valor e: {valor:.3f}")
