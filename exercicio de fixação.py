ead7d0d69ac48d9bcb21ae562d606e7f80dd51d7
unknown <aluno@IBC-LAB01-23.CETAM.LOCAL>
2026-09-17 20:18:34 -0400

    envio de arquivo

--- exercicio de fixação.py
+++ exercicio de fixação.py
@@ -1,31 +1,62 @@
 contador = 1
 
-while contador<=5:
+while contador<=1:
     print(contador)
     contador = contador + 1
+
+senha =""
+while senha!= "1234":
+    senha = input(" Digite a senha: ")
+    print ("Acesso permitido!")
+    quantidade= int(input("Quantos alunos? " ))
+
+contador= 1
+while contador <= quantidade:
+    nome = input("Digite o nome do aluno:")
+    print("Aluno cadastrado:" , nome)
+    contador = contador + 1
+
+opcao = 0
+while opcao != 1:
+    print("1 - Cadastrar aluno")
+    print("2 - listar alunos")
+    print("3 - Sair") 
+
+    opcao = int(input("Escolha uma opção: " ))
+
+    if opcao == 1:
+            print("cadastro de aluno")
+    elif opcao == 2:
+            print("Lista de alunos")
+    elif opcao == 3:
+            print("saindo...")
+    else:
+            print("opçao invalida!")
+    quantidade = int(input("Quantos livros deseja cadastrar? "))
     print()
+contador = 1 
+while contador <=1:
+     idade = int(input("Digite a idade:"))
+     if idade >= 18:
+       print("Maior de idade")
+     else:
+         print("Menor de idade")
+     contador = contador + 1
 
-    senha =""
-    while senha!= "1234":
-        senha = input(" Digite a senha: ")
-        print ("Acesso permitido!")
-        print()
-        quantidade= int(input("Quantos alunos? " ))
+nota = float(input("Digite uma nota de 0 a 10: "))
+while nota < 0 or nota >10:
+    print ("nota invalida!")
+    nota = float(input("Digite uma nota de 0 a 10: "))
+    print("Nota registrada:" , nota)
 
-        contador= 1
-        while contador <= quantidade:
-            nome = input("Digite o nome do aluno:")
-            print("Aluno cadastrado:" , nome)
-            contador = contador + 1
 
-            opão = 0
-            print()
-            while opcao != 3:
-                print("1 - Cadastrar aluno")
-                print("2 - listar alunos")
-                print("3 - Sair")
 
-                opcao = 
+
+
+
+
+
+
 
 
 
